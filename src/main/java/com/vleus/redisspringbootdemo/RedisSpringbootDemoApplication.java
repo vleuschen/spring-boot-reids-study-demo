49d6e52a65c36d9effbda62e1de03c6c8d20d37d
vleuschen <10719217@qq.com>
2021-07-04 11:16:41 +0800

    spring-boot整合reids的学习

---
+++ src/main/java/com/vleus/redisspringbootdemo/RedisSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vleus.redisspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vleus/redisspringbootdemo/RedisSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vleus.redisspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
